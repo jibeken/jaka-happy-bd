b87ceb85f42c61b57899e9adbc6aaff8f07454d3
jikuloveschicken <kenjibek1606@icloud.com>
2024-12-29 16:28:55 +0600

    Update happy-birthday.js

--- happy-birthday.js
+++ happy-birthday.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["ЖАРКЫНАЙ", "С ДНЕМ", "РОЖДЕНИЯ","(29 декабря - международный день", "биологического разнообразия)"],
+    strings: ["ЖАРКЫНАЙ", "С ДНЕМ", "РОЖДЕНИЯ"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
